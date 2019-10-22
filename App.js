f739a541206509f2a7bbf73c01c4cb5b9900c12d
jonatas <jonatastdb_jp@hotmail.com>
2019-10-22 00:26:52 -0300

    create Input component

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput
+
 } from 'react-native';
 
 import {
@@ -24,51 +26,16 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+import Input from './components/Input'
+
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", fontSize: 55 }}>
+        <Text style={{fontSize: 55}}>Hello, world! </Text>
+        <Text style={{fontSize: 55, color: 'blue'}}>I'm YOUPLUV! </Text>
+        {/* <TextInput style={{backgroundColor: 'red', width: 50}}></TextInput> */}
+        <Input></Input>
+      </View>
   );
 };
 
