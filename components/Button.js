f1090938e605c81c0f098ec3d162ec084ed9c3e6
Jaja <jarilson.rk@gmail.com>
2019-10-31 22:22:06 -0300

    Create Button Component

--- components/Button.js
+++ components/Button.js
@@ -3,19 +3,32 @@ import {TouchableOpacity, Text} from 'react-native'
 import styled from 'styled-components'
 import Colors from '../constants/Colors'
 
+
+
 export default Button = (props) => {
+    const {value= 'ENTRAR'}= props;
     return <ContainerButton onPress={props.onPress}>
-                <Label>ENTRAR</Label>
+                <Label>{value}</Label>
             </ContainerButton>
 }
 
 const ContainerButton = styled.TouchableOpacity`
-    height: 30px;
-    width: 100px;
-    background-color: yellow;
-    justify-content: center;
-    align-items: center;
+   width: 140px;
+   height: 40px;
+   top: 85px;
+   margin-bottom: 80px;
+   border-radius: 20px;
+   padding: 0px 20px;
+   background-color: #DFE8EB;
+   align-items: center;
+   justify-content: center;
 `
 const Label = styled.Text`
-    color: ${Colors.primaryColor};
+   font-size: 14px;
+   text-align: center;
+   justify-content: center;
+   font-style: normal;
+   font-weight: 600;
+   line-height: 17;
+   color: #292929;
 `
