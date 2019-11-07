3274817e3bd59f392842b7c0776c9a9f5d50145a
Alan Araujo <alan.techsafe@hotmail.com>
2019-11-07 01:11:37 -0300

    Implement register layout

--- screens/Register/Register.js
+++ screens/Register/Register.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
+import React from "react";
+import { StyleSheet, View, Image, TouchableHighlight, Text } from "react-native";
 
-import Button from '../../components/Button';
+import { ContainerForm, Row } from "../../components/Structure";
+import logo from "../../assets/images/logo.png";
+import Button from "../../components/Button";
+import Input from "../../components/Input/Input";
+import CustomGradient from "../../components/CustomGradient";
 
 export default function Register(props) {
   return (
-      <View
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-       
-          <Button  onPress={() => props.navigation.navigate('Main')}/>
-
-      </View>
+    <CustomGradient style={styles.container} >
+      <ContainerForm>
+        <Input placeholder="Nome"/>
+        <Input placeholder="Email"/>
+        <Input placeholder="Senha" />
+        <Input placeholder="Confirmar Senha" />
+        
+        <Button width={"100%"} onPress={() => props.navigation.navigate("Main")} value={"CRIAR CONTA"}/>
+      </ContainerForm>
+    </CustomGradient>
   );
 }
 
-Register.navigationOptions = {
-  header: null,
-};
+// Register.navigationOptions = {
+//   header: null,
+// };
 
 const styles = StyleSheet.create({
+  logo: {
+    width: 400,
+    maxWidth: "70%",
+    height: 100,
+    marginBottom: 50
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#a7e9ff",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  forgotPassword: {
+    alignSelf: "flex-end",
+    color: "#fff",
+  },
+  withoutAccount: {
+    color: "#fff",
+  },
+  register: {
+    fontWeight: "900",
+    color: "#fff",
   }
 });
