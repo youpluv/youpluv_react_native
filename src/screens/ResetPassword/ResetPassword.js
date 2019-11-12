0927001e833168e27877b2ca3f1dcda9e38f6354
Gaby <lubaaenzin@gmail.com>
2019-11-11 22:07:56 -0200

    modalResetPassword

--- src/screens/ResetPassword/ResetPassword.js
+++ src/screens/ResetPassword/ResetPassword.js
@@ -1,40 +1,66 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import {
   StyleSheet,
   View,
   Text,
+  Modal,
+  Image,
+  TouchableHighlight
 } from 'react-native';
-import { FormResetPass, ContainerForm } from "../../components/Structure";
+import { ContainerForm } from "../../components/Structure";
 import Button from '../../components/Button';
 import Input from "../../components/Input/Input";
+import iconX from "../../assets/images/iconX.png"
+import iconCheck from "../../assets/images/iconCheck.png"
 
 export default function ResetPassword(props) {
+
+  const [modalVisible, setmodalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setmodalVisible(!modalVisible);
+ }
+
   return (
-      <View
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        
-        <View style={styles.textContainer}>
-          <Text style={styles.TextsStyles2}>Esqueceu sua senha?</Text>
-          <Text style={styles.TextsStyles2}>É fácil resolver.</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.TextsStyles}>Basta digitar o email cadastrado e clicar em </Text>
-          <Text style={styles.TextsStyles2}>RECUPERAR SENHA.</Text>
-          <Text style={styles.TextsStyles}>Em instantes você receberá o link no seu email.</Text>
-        </View>
-
-
-        <ContainerForm>
-        <Text style={styles.TextsStyles3}>Insira seu email no campo abaixo.</Text>
-        
-          <Input />
-          <Button width="100%" value={"RECUPERAR SENHA"} onPress={() => props.navigation.navigate('Main')}/>
-        </ContainerForm>
+    <View
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}>
+      
+      <View style={styles.textContainer}>
+        <Text style={styles.TextsStyles2}>Esqueceu sua senha?</Text>
+        <Text style={styles.TextsStyles2}>É fácil resolver.</Text>
       </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.TextsStyles}>Basta digitar o email cadastrado e clicar em </Text>
+        <Text style={styles.TextsStyles2}>RECUPERAR SENHA.</Text>
+        <Text style={styles.TextsStyles}>Em instantes você receberá o link no seu email.</Text>
+      </View>
+
+      <ContainerForm>
+        <Text style={styles.TextsStyles3}>Insira seu email no campo abaixo.</Text>        
+        <Input />
+        <Button width="100%" value={"RECUPERAR SENHA"} onPress={() => toggleModal()}/>
+      </ContainerForm>
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible = {modalVisible}>
+            <View style={styles.BackgroundModal}>
+            <TouchableHighlight style={{position: 'absolute',left:304,top: 24,}} onPress={()=>toggleModal()}>
+              <Image style={{width:40, height:40}} source={iconX}/>
+            </TouchableHighlight>  
+              <View style={styles.ModalStyle}>
+              <Image style={{width:50, height:50}} source={iconCheck}/>
+                <Text style={styles.TextModal1}>O email para recuperar sua senha foi enviado.</Text>
+                <Text style={styles.TextModal2}>Enviamos para o email ad********i@gmail.com as instruções para recuperar a sua senha.</Text>
+              </View>
+            </View>
+        </Modal>     
+
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -50,17 +76,60 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   TextsStyles:{
+    fontFamily: "montserrat-regular",
     color:'#FFFFFF',
-    fontSize:15,
+    fontSize:14,
   },
   TextsStyles2:{
+    fontFamily: "montserrat-regular",
     color:'#FFFFFF',
     fontSize:18,
     fontWeight:"500",
   },
   TextsStyles3:{
+    fontFamily: "montserrat-regular",
     marginVertical:10,
     color:'#FFFFFF',
     fontSize:15,
+  },
+  BackgroundModal:{
+    width:400, 
+    height:800, 
+    backgroundColor: 'rgba(0, 0, 0, 0.53)'
+  },
+  ModalStyle:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7F7F7',
+    width:300,
+    height:200,
+    borderRadius:30,
+    marginTop:200,
+    marginLeft:30,
+    marginRight:30,
+  },
+  TextModal1:{
+    padding:10,
+    fontFamily: 'montserrat-regular',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 22,
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#116682',
+  },
+  TextModal2:{
+    padding:5,
+    fontFamily: 'montserrat-regular',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 14,
+    lineHeight: 20,
+    alignItems: 'center',
+    textAlign: 'center',
+    color:'#235A6E',
+    minWidth:100,
+    width:'90%',
   }
 });
