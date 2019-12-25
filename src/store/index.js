75d115be7cf30ab371939f75571fb06074a10f5c
Alan Araujo <alan.techsafe@hotmail.com>
2019-12-25 13:01:44 -0300

    redux-persist implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunk from 'redux-thunk';
-import userReducer from './user/user.reducer';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+import userReducer from "./user/user.reducer";
+import { AsyncStorage } from "react-native";
+
+const persistConfig = {
+  //...
+  key: "root",
+  storage: AsyncStorage
+};
 
 // Note: this API requires redux@>=3.1.0
 const rootReducer = combineReducers({
-    user: userReducer
-})
+  user: userReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [thunk];
-  const composeEnhancers =
-    global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const composeEnhancers = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default store = createStore(rootReducer, {}, enhancer);
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+let persistor = persistStore(store);
+
+export { store, persistor };
