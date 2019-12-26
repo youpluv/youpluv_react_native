e728df93a7a7f74c05533440a439423d174ab0e1
Jaja <jarilson.rk@gmail.com>
2019-12-26 18:38:27 -0300

    Create componente LineDivisor

---
+++ src/components/LineDivisor/LineDivisor.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import {  View,} from "react-native";
+import { Container, Line, H2 } from "./styles";
+
+export default function LineDivisor() {
+  return (
+    <Container>
+      <Line></Line>
+      <H2>OU</H2>
+      <Line></Line>
+    </Container>
+  );
+}
---
+++ src/components/LineDivisor/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+// import { Dimensions } from 'react-native';
+
+export const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 21px;
+`;
+export const Line = styled.View`
+  height: 1px;
+  width: 130px;
+  background-color: white;
+`;
+
+export const H2 = styled.Text`
+  margin: 5px;
+  width: 30px;
+  color: #fff;
+  height: 21px;
+  font-size: 18px;
+  align-items: center;
+  justify-content: center;
+  font-family: "montserrat-regular";
+  /* margin-left: ; */
+  /* margin-rigth:  */
+  /* line-height: 22; */
+`;
