7d90fd67153829ad088e6470a85cfced27424dac
Jaja <jarilson.rk@gmail.com>
2020-01-02 20:16:47 -0300

    wip

--- src/components/LoginSocial/LoginSocial.js
+++ src/components/LoginSocial/LoginSocial.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as Facebook from "expo-facebook";
 
 import {
   Container,
@@ -12,11 +13,35 @@ import { useDipatch, useDispatch } from "react-redux";
 import { socialLogin } from "../../store/user/user.actions";
 export default function LoginSocial() {
   const dispatch = useDispatch();
-
+  async function logInFB() {
+    try {
+      await Facebook.initializeAsync("641247119948894");
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile"]
+      });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        );
+        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
   return (
     <Container>
       <Content>
-        <Circle>
+        <Circle onPress={() => logInFB()}>
           <IconFacebook name="facebook-f" />
         </Circle>
         <H2>Entrar com o Facebook</H2>
