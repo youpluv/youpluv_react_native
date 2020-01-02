94b7e29c509a266567181ee0fd5a80e4dfbac764
angelo <angelo@techdog.com.br>
2020-01-01 22:17:40 -0300

    get position user

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Button from "../../components/Button";
@@ -9,9 +9,47 @@ import { NavigationActions, StackActions } from "react-navigation";
 import DayNight from "./DayNight";
 import Layout from "../../constants/Layout";
 import WeatherCard from "../../components/WeatherCard/WeatherCard";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default function Home(props) {
   const dispatch = useDispatch();
+
+  // state = {
+  //   location: null,
+  //   errorMessage: null
+  // };
+
+  useEffect(() => {
+    // if (Platform.OS === "android" && !Constants.isDevice) {
+    //   this.setState({
+    //     errorMessage:
+    //       "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+    //   });
+    // } else {
+    _getLocationAsync();
+    // }
+  }, []);
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({}).then(location => {
+      Location.reverseGeocodeAsync({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }).then(region => {
+        console.log(region, "location");
+      });
+      console.log(location, "location atual");
+    });
+    // this.setState({ location });
+  };
 
   const logout = () => {
     const resetAction = StackActions.reset({
