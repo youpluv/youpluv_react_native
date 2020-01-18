fd10749dbea364b4cb0277ed036dd091b5aaf67d
Jaja <qualquercoisa333133127@gmail.com>
2020-01-18 20:32:36 -0300

    Update

--- src/components/CustomGradient.js
+++ src/components/CustomGradient.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react";
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function CustomGradient(props) {
+  const { colors = ["#A7E9FF", "#015D7B"] } = props;
   return (
-    <LinearGradient colors={['#A7E9FF', '#015D7B']} style={props.style}>
-        {props.children}
-    </LinearGradient>    
+    <LinearGradient colors={colors} style={props.style}>
+      {props.children}
+    </LinearGradient>
   );
 }
