7bc7e0695639bb462b12b66fc9118d35390b18a9
Alan Araujo <alan.techsafe@hotmail.com>
2020-01-30 23:36:53 -0300

    Change url base

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from "react-native";
 
 export const axiosInstance = async () => {
   const instance = Axios.create({
-    baseURL: "https://youpluv.herokuapp.com/",
+    baseURL: "https://youpluv.herokuapp.com/api/",
     headers: {
       Authorization: `Bearer ${await AsyncStorage.getItem("token")}`
     }
